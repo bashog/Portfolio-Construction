12891f897dd3ac7117a41eb822147d41343212b2
lilhugo <hugo.fourel@yahoo.com>
2022-12-05 16:16:37 +0100

    Ajout des tickers à garder

---
+++ tickers_keep.py
@@ -0,0 +1,9 @@
+['ASML', 'MC.PA', 'NVS', 'SAP', 'CFR.SW', 'ABB', 'RMS.PA', 'KER.PA',
+       'RI.PA', 'ISP.MI', 'CAP.PA', 'IDEXF', '6S=F', 'AMS.MC', 'SAMPO.HE',
+       'ADDYY', 'LR.PA', 'G.MI', 'VNA.DE', 'KNEBV.HE', 'CABK.MC',
+       'PUB.PA', 'COLO-B.CO', 'SHB-A.ST', 'SCMN.SW', 'KBC.BR', 'HNR1.DE',
+       'QGEN', 'BNR.DE', 'TRN.MI', 'EBS.VI', 'AENA.MC', 'EXO.AS',
+       'GBLC.CN', 'RDFN', 'BVI.PA', 'TELIA.ST', 'RAND.AS', 'ZAL.DE',
+       'SIG', 'VIV.PA', 'PNDORA.CO', 'JMT.LS', 'WFDRX', 'MB.MI', 'ATL.MI',
+       'LI.PA', 'BOL.PA', 'DIE.BR', 'GFC.PA', 'SECU-B.ST', 'IDVA.SG',
+       'FR.PA', 'LBGUF', 'COV.PA', 'MF.PA']
